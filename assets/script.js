88a89a3044008c2eead7528e8d2d7ba3c388d208
Jay Ford <jay.ford45@gmail.com>
2022-02-17 17:48:53 -0500

    cleaned up code and edited writepassword function

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,19 @@
 // Assignment code here
 
-let length = function(){
+let length = function () {
   let lengthPrompt = window.prompt("How many characters would you like your password to be? Please pick a number between 8 and 128.");
   lengthPrompt = parseInt(lengthPrompt);
-  if (lengthPrompt >= 8 && lengthPrompt <= 128){
+  if (lengthPrompt >= 8 && lengthPrompt <= 128) {
     return lengthPrompt;
   }
-  else{
+  else {
     window.alert("That was not a valid answer. Please try again.")
     length();
   }
 }
 
 
-var generatePassword = function(){
+var generatePassword = function () {
   let result = "";
   let capChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   let lowChars = "abcdefghijklmnopqrstuvwxyz";
@@ -24,81 +24,81 @@ var generatePassword = function(){
   let numConfirm = window.confirm("Use numbers?");
   let specialConfirm = window.confirm("Use special characters?")
   let lengthPrompt = length();
-  
+
 
   if (capConfirm && lowConfirm && numConfirm && specialConfirm) {
     for (var i = 0; i < lengthPrompt; i++) {
-      result += (capChars + lowChars + numbers + special).charAt(Math.floor(Math.random()*(capChars + lowChars + numbers + special).length));
+      result += (capChars + lowChars + numbers + special).charAt(Math.floor(Math.random() * (capChars + lowChars + numbers + special).length));
     }
   }
   else if (capConfirm && lowConfirm && numConfirm && specialConfirm === false) {
     for (var i = 0; i < lengthPrompt; i++) {
-      result += (capChars + lowChars + numbers).charAt(Math.floor(Math.random()*(capChars + lowChars + numbers).length));
+      result += (capChars + lowChars + numbers).charAt(Math.floor(Math.random() * (capChars + lowChars + numbers).length));
     }
   }
   else if (capConfirm && lowConfirm && numConfirm === false && specialConfirm === false) {
     for (var i = 0; i < lengthPrompt; i++) {
-      result += (capChars + lowChars).charAt(Math.floor(Math.random()*(capChars + lowChars).length));
+      result += (capChars + lowChars).charAt(Math.floor(Math.random() * (capChars + lowChars).length));
     }
   }
   else if (capConfirm && lowConfirm === false && numConfirm === false && specialConfirm === false) {
     for (var i = 0; i < lengthPrompt; i++) {
-      result += (capChars).charAt(Math.floor(Math.random()*(capChars).length));
+      result += (capChars).charAt(Math.floor(Math.random() * (capChars).length));
     }
   }
   else if (capConfirm === false && lowConfirm && numConfirm && specialConfirm) {
     for (var i = 0; i < lengthPrompt; i++) {
-      result += (lowChars + numbers + special).charAt(Math.floor(Math.random()*lengthPrompt));
+      result += (lowChars + numbers + special).charAt(Math.floor(Math.random() * lengthPrompt));
     }
   }
   else if (capConfirm === false && lowConfirm === false && numConfirm && specialConfirm) {
     for (var i = 0; i < lengthPrompt; i++) {
-      result += (numbers + special).charAt(Math.floor(Math.random()*lengthPrompt));
+      result += (numbers + special).charAt(Math.floor(Math.random() * lengthPrompt));
     }
   }
-  else if (capConfirm === false && lowConfirm && numConfirm && specialConfirm === false){
+  else if (capConfirm === false && lowConfirm && numConfirm && specialConfirm === false) {
     for (var i = 0; i < lengthPrompt; i++) {
-      result += (lowChars + numbers).charAt(Math.floor(Math.random()*lengthPrompt));
+      result += (lowChars + numbers).charAt(Math.floor(Math.random() * lengthPrompt));
     }
   }
-  else if (capConfirm === false && lowConfirm && numConfirm === false && specialConfirm){
+  else if (capConfirm === false && lowConfirm && numConfirm === false && specialConfirm) {
     for (var i = 0; i < lengthPrompt; i++) {
-      result += (lowChars + special).charAt(Math.floor(Math.random()*lengthPrompt));
+      result += (lowChars + special).charAt(Math.floor(Math.random() * lengthPrompt));
     }
   }
-  else if (capConfirm === false && lowConfirm && numConfirm === false && specialConfirm === false){
+  else if (capConfirm === false && lowConfirm && numConfirm === false && specialConfirm === false) {
     for (var i = 0; i < lengthPrompt; i++) {
-      result += (lowChars).charAt(Math.floor(Math.random()*lengthPrompt));
+      result += (lowChars).charAt(Math.floor(Math.random() * lengthPrompt));
     }
   }
-  else if (capConfirm && lowConfirm && numConfirm === false && specialConfirm){
+  else if (capConfirm && lowConfirm && numConfirm === false && specialConfirm) {
     for (var i = 0; i < lengthPrompt; i++) {
-      result += (capChars + lowChars + special).charAt(Math.floor(Math.random()*lengthPrompt));
+      result += (capChars + lowChars + special).charAt(Math.floor(Math.random() * lengthPrompt));
     }
   }
-  else if (capConfirm && lowConfirm === false && numConfirm && specialConfirm){
+  else if (capConfirm && lowConfirm === false && numConfirm && specialConfirm) {
     for (var i = 0; i < lengthPrompt; i++) {
-      result += (capChars + numbers + special).charAt(Math.floor(Math.random()*lengthPrompt));
+      result += (capChars + numbers + special).charAt(Math.floor(Math.random() * lengthPrompt));
     }
   }
-  else if (capConfirm && lowConfirm === false && numConfirm && specialConfirm === false){
+  else if (capConfirm && lowConfirm === false && numConfirm && specialConfirm === false) {
     for (var i = 0; i < lengthPrompt; i++) {
-      result += (capChars + numbers).charAt(Math.floor(Math.random()*lengthPrompt));
+      result += (capChars + numbers).charAt(Math.floor(Math.random() * lengthPrompt));
     }
   }
-  else if (capConfirm === false && lowConfirm && numConfirm === false && specialConfirm){
+  else if (capConfirm === false && lowConfirm && numConfirm === false && specialConfirm) {
     for (var i = 0; i < lengthPrompt; i++) {
-      result += (capChars + numbers + special).charAt(Math.floor(Math.random()*lengthPrompt));
+      result += (capChars + numbers + special).charAt(Math.floor(Math.random() * lengthPrompt));
     }
   }
-  else if (capConfirm === false && lowConfirm === false && numConfirm && specialConfirm === false){
+  else if (capConfirm === false && lowConfirm === false && numConfirm && specialConfirm === false) {
     for (var i = 0; i < lengthPrompt; i++) {
-      result += (numbers).charAt(Math.floor(Math.random()*lengthPrompt));
+      result += (numbers).charAt(Math.floor(Math.random() * lengthPrompt));
     }
   }
-  else if (capConfirm === false && lowConfirm === false && numConfirm === false && specialConfirm){
+  else if (capConfirm === false && lowConfirm === false && numConfirm === false && specialConfirm) {
     for (var i = 0; i < lengthPrompt; i++) {
-      result += (special).charAt(Math.floor(Math.random()*lengthPrompt));
+      result += (special).charAt(Math.floor(Math.random() * lengthPrompt));
     }
   }
   else {
@@ -115,9 +115,9 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  document.getElementById("password").innerHTML = password;
+  var passwordText = document.getElementById("password");
 
-  //passwordText.value = password;
+  passwordText.value = password;
 
 }
 
