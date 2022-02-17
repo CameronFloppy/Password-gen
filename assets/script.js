6854c47124cb5769ea42aa5743429880e3b1bbac
Jay Ford <jay.ford45@gmail.com>
2022-02-17 17:46:17 -0500

    fixed functionality to allow password to display in the text area

--- assets/script.js
+++ assets/script.js
@@ -115,7 +115,7 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.getElementById("#password");
+  document.getElementById("password").innerHTML = password;
 
   //passwordText.value = password;
 
